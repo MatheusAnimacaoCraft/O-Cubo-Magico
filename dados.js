48398f7a303bed028c1cbc7b676686c081ac0c86
MatheusAnimacaoCraft <matheuscustodio706@gmail.com>
2024-09-06 22:38:34 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,17 @@
+let dados = [
+    {
+      titulo: "Cubo Mágico",
+      descricao: "O Cubo Mágico, também conhecido como Cubo de Rubik, é um quebra-cabeça tridimensional que desafia a mente e a coordenação motora de pessoas de todas as idades. Inventado pelo húngaro Ernő Rubik em 1974, esse cubo colorido é composto por seis faces, cada uma com nove quadrados de cores diferentes.",
+      link: "https://pt.wikipedia.org/wiki/Cubo_de_Rubik"
+    },
+    {
+      titulo: "Megaminx",
+      descricao: "O Megaminx é uma variação do Cubo Mágico com 12 faces pentagonais e 50 peças móveis. É considerado um dos quebra-cabeças mais complexos da sua categoria, exigindo um alto nível de habilidade para ser resolvido.",
+      link: "https://pt.wikipedia.org/wiki/Megaminx"
+    },
+    {
+      titulo: "Pyraminx",
+      descricao: "O Pyraminx é um quebra-cabeça tetraédrico, com quatro faces triangulares. Apesar de sua aparência simples, ele oferece um desafio interessante para quem gosta de resolver cubos.",
+      link: "https://pt.wikipedia.org/wiki/Pyraminx"
+    }
+  ];
